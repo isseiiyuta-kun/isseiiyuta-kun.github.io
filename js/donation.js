7e81b9e2ebb0b68583eb717f055aac7603b61da7
isseiiyuta-kun <isseiiyuta@gmail.com>
2026-07-30 20:57:20 +0800

    Delete js directory

--- js/donation.js
+++
@@ -1,8 +0,0 @@
-const donateButton = document.querySelector(".donate-btn");
-
-donateButton.addEventListener("click", function(){
-
-    alert("🎉 Thank you for supporting Cleansing Impurities!");
-
-});
- 
